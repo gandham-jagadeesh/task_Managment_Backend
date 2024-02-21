6e99a369f91f07e248ba52a43dcd21fa5e578616
gandham-jagadeesh <gandhamphotos@gmail.com>
2024-02-22 00:59:45 +0530

    basic task crud operations completed

--- routes/tasksRoute.js
+++ routes/tasksRoute.js
@@ -1,12 +1,14 @@
 const router = require('express').Router();
 const userTasksModel = require('../models/userTaskModel'); 
-const {taskSchema,taskModel} = require('../models/taskModel');
+const {taskModel} = require('../models/taskModel');
 
+
+//get all the tasks
 router.get('/',async(req,res)=>{
     try{
         const email = req.body.email;
-        const allTasksData = await userTasksModel.find({email:email});
-        res.send({data:allTasksData[0].tasks});
+        const allTasksData = await userTasksModel.findOne({email:email});
+        res.send({data:allTasksData.tasks});
     }
     catch(e){
      res.status(500).send({'msg':'internal error '})
@@ -15,42 +17,85 @@ router.get('/',async(req,res)=>{
 });
 
 
-//add a task
+//adding a task
 router.post('/add',async(req,res)=>{
     try{
 
         const email = req.body.email;
         const task  = req.body.tasks;
-        const allTasksData = await userTasksModel.find({email:email});
+        const allTasksData = await userTasksModel.findOne({email:email});
         const newTask  = new taskModel(task);
+        console.log(allTasksData);
         if(allTasksData.length == 0){
             const newDoc   = new userTasksModel({email:email,tasks:[newTask]});
             await newDoc.save();
             res.status(200).send({msg:'added sucessfully'});
         }else{
-            const data = allTasksData[0]
-            const array = data.tasks;
+            const array = allTasksData.tasks;
             array.push(newTask);
+            console.log(array);
             const response = await userTasksModel.findOneAndUpdate({email:email},{tasks:array});
             res.status(200).send({task:response});
         }
     }
     catch(e){
+        console.log(e);
         res.status(400).send({'msg':'not inserted the task'});
     }
 });
 
 
 
-//update specific task
-router.post('/:id',async(req,res)=>{
-
+//updating specific task
+router.post('/update/:id',async(req,res)=>{
+ const email = req.body.email;
+ const id    = req.params.id;
+ const rawTask  = req.body.task;
+ const task     = new taskModel(rawTask);
+  try{
+    const allTasksData = await userTasksModel.findOne({email:email});
+    const modifiedTasks = allTasksData.tasks.map((each)=>{
+        console.log(String(each._id),id,String(each._id) === id);
+        if(String(each._id) == id){
+           return task
+        }
+        return each
+    })
+    await userTasksModel.updateOne({tasks:modifiedTasks});
+   res.status(200).send({msg:'update sucessfully',task:task});
+  }
+  catch(error){
+    console.log(error);
+  res.status(400).send({msg:"not updated"});
+  }
+  
 });
 
 
-//delete a specific task
-router.post('/del/:id',async(req,res)=>{
+//deleting a specific task
+    router.post('/del/:id',async(req,res)=>{
+    try{
+    const email = req.body.email;
+    const id    = req.params.id;
+    const allTasks   = await userTasksModel.findOne({email:email});
+    const modifiedTasks      = allTasks.tasks.filter((each)=>{
+        if(String(each._id) !== id){
+            return true
+        }
+        return false
+    })
+    await userTasksModel.updateOne({tasks:modifiedTasks});
+    res.status(200).send({msg:'deleted sucessfully'});
+    }
+    catch(error){
+    res.status(400).send({msg:"not deleted"})
+    }
 
-});
+    });
+
+
+
+
+
 
 module.exports = router;
